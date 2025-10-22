ea95258fbdb13c4bfc3ad3e4d0d00a10b3c3321a
arstanrakhmat <arstanbek.rakh@gmail.com>
2025-10-22 10:46:59 +0600

    Work with snackbar

--- app/src/main/java/ar/arstan/vkcustom/ui/theme/VkNewsMainScreen.kt
+++ app/src/main/java/ar/arstan/vkcustom/ui/theme/VkNewsMainScreen.kt
@@ -3,23 +3,64 @@ package ar.arstan.vkcustom.ui.theme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.NavigationBarItemColors
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.SnackbarResult
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import kotlinx.coroutines.launch
 
 @Composable
 fun MainScreen() {
+    val snackbarHostState = remember {
+        SnackbarHostState()
+    }
+
+    val scope = rememberCoroutineScope()
+    val fabIsVisible = remember {
+        mutableStateOf(true)
+    }
+
     Scaffold(
+        snackbarHost = {
+            SnackbarHost(hostState = snackbarHostState)
+        },
+        floatingActionButton = {
+            if (fabIsVisible.value) {
+                FloatingActionButton(onClick = {
+                    scope.launch {
+                        val action = snackbarHostState.showSnackbar(
+                            message = "This is snackbar",
+                            actionLabel = "Hide FAB",
+                            duration = SnackbarDuration.Long
+                        )
+
+                        if (action == SnackbarResult.ActionPerformed) {
+                            fabIsVisible.value = false
+                        }
+                    }
+                }) {
+                    Icon(imageVector = Icons.Filled.Favorite, contentDescription = null)
+                }
+            }
+        },
         bottomBar = {
             val selectedItemPosition = remember {
                 mutableIntStateOf(0)
