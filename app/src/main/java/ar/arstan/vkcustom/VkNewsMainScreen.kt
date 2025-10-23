18408fbaf357f9d69393f06c0adc0cb1166de4b5
Arstanbek Rakhmatulaev <arstanbek.rakh@gmail.com>
2025-10-23 11:53:39 +0600

    Navigation refactor

--- app/src/main/java/ar/arstan/vkcustom/VkNewsMainScreen.kt
+++ app/src/main/java/ar/arstan/vkcustom/VkNewsMainScreen.kt
@@ -1,6 +1,8 @@
 package ar.arstan.vkcustom
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
@@ -10,7 +12,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -18,6 +20,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import ar.arstan.vkcustom.navigation.AppNavGraph
+import ar.arstan.vkcustom.navigation.Screen
 import ar.arstan.vkcustom.ui.theme.Black500
 import ar.arstan.vkcustom.ui.theme.Black900
 
@@ -40,7 +43,15 @@ fun MainScreen(viewModel: MainViewModel) {
                 items.forEach { item ->
                     NavigationBarItem(
                         selected = currentRoute == item.screen.route,
-                        onClick = { navHostController.navigate(route = item.screen.route) },
+                        onClick = {
+                            navHostController.navigate(route = item.screen.route) {
+                                popUpTo(Screen.NewsFeed.route) {
+                                    saveState = true
+                                }
+                                launchSingleTop = true
+                                restoreState = true
+                            }
+                        },
                         icon = {
                             Icon(item.icon, contentDescription = null)
                         },
@@ -64,21 +75,29 @@ fun MainScreen(viewModel: MainViewModel) {
         AppNavGraph(
             navHostController = navHostController,
             homeScreenContent = { HomeScreen(viewModel = viewModel, paddingValues = innerPadding) },
-            favoriteScreenContent = { TextCounter(name = "Favorite") },
-            profileScreenContent = { TextCounter(name = "Profile") }
+            favoriteScreenContent = {
+                TextCounter(
+                    name = "Favorite",
+                    paddingValues = innerPadding
+                )
+            },
+            profileScreenContent = { TextCounter(name = "Profile", paddingValues = innerPadding) }
         )
     }
 }
 
 @Composable
 private fun TextCounter(
-    name: String
+    name: String,
+    paddingValues: PaddingValues
 ) {
-    var count by remember {
+    var count by rememberSaveable {
         mutableStateOf(0)
     }
     Text(
-        modifier = Modifier.clickable { count++ },
+        modifier = Modifier
+            .padding(paddingValues = paddingValues)
+            .clickable { count++ },
         text = "$name Count: $count",
         color = Black900
     )
